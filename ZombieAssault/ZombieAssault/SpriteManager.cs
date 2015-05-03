19242036b9e39b99f1b894ada2b7824642ebff6e
AnonymousGnome <toste3@hotmail.com>
2015-05-03 09:27:30 -0500

    Added buttons to the start menu. Controls button doesn't work yet.

--- ZombieAssault/ZombieAssault/SpriteManager.cs
+++ ZombieAssault/ZombieAssault/SpriteManager.cs
@@ -22,7 +22,7 @@ namespace ZombieAssault
         PlayerManager playerManager;
         BreakableObjectManager breakableObjectManager;
 
-        public static readonly float scaleFactor = Game1.resHeight/960f;//factor by which sprites will be scaled to, based on resolution
+        public static readonly float scaleFactor = Game1.resHeight / 960f;//factor by which sprites will be scaled to, based on resolution
         public static readonly float tileSize = scaleFactor * 24;
         public static readonly float gridOffset = ((float)Game1.resHeight / Game1.resWidth) * tileSize + 1;
 
@@ -33,10 +33,22 @@ namespace ZombieAssault
         private Texture2D unitHudTexture;
         private Texture2D healthbarGreenTexture;
         private Texture2D healthbarRedTexture;
+        private Texture2D startButtonTexture;
+        private Texture2D exitButtonTexture;
+        private Texture2D controlsButtonTexture;
+
         private Vector2 cursorPosition;
+        private Vector2 screenCenter;
+
         private int gameState;
 
+        private Rectangle startButtonRectangle;
+        private Rectangle exitButtonRectangle;
+        private Rectangle controlsButtonRectangle;
         private Rectangle jackHealthBar;
+
+        private MouseState mouseState;
+        private MouseState previousMouseState;
 
         public int GameState
         {
@@ -70,19 +82,32 @@ namespace ZombieAssault
             unitHudTexture = Game.Content.Load<Texture2D>(@"Images/HUD/Hud_UnitInfo");
             healthbarGreenTexture = Game.Content.Load<Texture2D>(@"Images/HUD/HealthBar/HealthBar");
             healthbarRedTexture = Game.Content.Load<Texture2D>(@"Images/HUD/HealthBar/HealthBarUnder");
+            startButtonTexture = Game.Content.Load<Texture2D>(@"Images/Buttons/StartButton");
+            exitButtonTexture = Game.Content.Load<Texture2D>(@"Images/Buttons/ExitButton");
+            controlsButtonTexture = Game.Content.Load<Texture2D>(@"Images/Buttons/ControlsButton");
+
+            screenCenter = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+
+            startButtonRectangle = new Rectangle((int)(screenCenter.X - 100 * scaleFactor), (int)(screenCenter.Y - 50 * scaleFactor), 200 * (int)scaleFactor, 60 * (int)scaleFactor);
+            controlsButtonRectangle = new Rectangle((int)(screenCenter.X - 100 * scaleFactor), (int)(screenCenter.Y + 50 * scaleFactor), 200 * (int)scaleFactor, 60 * (int)scaleFactor);
+            exitButtonRectangle = new Rectangle((int)(screenCenter.X - 100 * scaleFactor), (int)(screenCenter.Y + 150 * scaleFactor), 200 * (int)scaleFactor, 60 * (int)scaleFactor);
 
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
-            foreach(Sprite s in spriteList)//runs update method of each sprite in the list
+            //wait for mouseclick
+            previousMouseState = mouseState;
+            mouseState = Mouse.GetState();
+            if (previousMouseState.LeftButton == ButtonState.Pressed && mouseState.LeftButton == ButtonState.Released)
+            {
+                 MouseClicked(mouseState.X, mouseState.Y);
+            }
+
+            foreach (Sprite s in spriteList)//runs update method of each sprite in the list
             {
                 s.Update(gameTime, Game.Window.ClientBounds);
-            }
-            if(GameState == 0 && Keyboard.GetState().IsKeyDown(Keys.Enter))
-            {
-                GameState = 1;
             }
             if (GameState == 1)
             {
@@ -92,13 +117,13 @@ namespace ZombieAssault
                 if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                     GameState = 2;
             }
-            if(gameState == 2 && Keyboard.GetState().IsKeyDown(Keys.Enter))
+            if (gameState == 2 && Keyboard.GetState().IsKeyDown(Keys.Enter))
             {
                 GameState = 1;
             }
 
             cursorPosition = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
-            
+
             base.Update(gameTime);
         }
 
@@ -108,9 +133,12 @@ namespace ZombieAssault
 
             spriteBatch.Draw(cursorTexture, cursorPosition, null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 1);//draws cursor
 
-            if(GameState == 0)
+            if (GameState == 0)
             {
                 spriteBatch.Draw(titleTexture, new Vector2(Game1.resOffset, 0), null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 0);
+                spriteBatch.Draw(startButtonTexture, startButtonRectangle, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, .5f);
+                spriteBatch.Draw(controlsButtonTexture, controlsButtonRectangle, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, .5f);
+                spriteBatch.Draw(exitButtonTexture, exitButtonRectangle, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, .5f);
             }
             else if (GameState == 1 || GameState == 2)
             {
@@ -118,7 +146,7 @@ namespace ZombieAssault
                     s.Draw(gameTime, spriteBatch);
                 spriteBatch.Draw(mapTexture, new Vector2(Game1.resOffset, 0), null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 0);//draws map
                 spriteBatch.Draw(highlightTexture, new Vector2((int)((cursorPosition.X - gridOffset) / tileSize) * tileSize + gridOffset, (int)(cursorPosition.Y / tileSize) * tileSize), null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, .2f);//draws tile highlight
-                spriteBatch.Draw(unitHudTexture, new Vector2((int)(Game1.resOffset - 315 * scaleFactor), 0), null, Color.White, 0, Vector2.Zero, scaleFactor,  SpriteEffects.None, 1);
+                spriteBatch.Draw(unitHudTexture, new Vector2((int)(Game1.resOffset - 315 * scaleFactor), 0), null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 1);
                 spriteBatch.Draw(unitHudTexture, new Vector2((int)(Game1.resWidth - Game1.resOffset /*+ 315 * SpriteManager.scaleFactor*/), 0), null, Color.White, 0, Vector2.Zero, scaleFactor, SpriteEffects.None, 1);
                 spriteBatch.Draw(healthbarGreenTexture, jackHealthBar, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 1);
                 spriteBatch.Draw(healthbarRedTexture, new Rectangle(jackHealthBar.X, jackHealthBar.Y, 152 * (int)scaleFactor, 40 * (int)scaleFactor), null, Color.White, 0, Vector2.Zero, SpriteEffects.None, .9f);
@@ -133,6 +161,24 @@ namespace ZombieAssault
 
             spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        private void MouseClicked(int x, int y)
+        {
+            //creates a rectangle of 1x1 around the place where the mouse was clicked
+            Rectangle mouseClickRect = new Rectangle(x, y, 1, 1);
+
+            if (gameState == 0)
+            {
+                if (mouseClickRect.Intersects(startButtonRectangle)) //player clicked start button
+                {
+                    gameState = 1;
+                }
+                else if (mouseClickRect.Intersects(exitButtonRectangle)) //player clicked exit button
+                {
+                    Game.Exit();
+                }
+            }
         }
     }
 }
